e3f5c3af00739bdd93bcf46db1e8b377aed9336d
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-17 08:38:38 +0100

    Fixed exceptions hierarchy tests

--- Doctrineum/Tests/Float/DoctrineumFloatExceptionsHierarchyTest.php
+++ Doctrineum/Tests/Float/DoctrineumFloatExceptionsHierarchyTest.php
@@ -2,20 +2,29 @@
 namespace Doctrineum\Tests\Float;
 
 use Doctrineum\Scalar\ScalarEnum;
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
-class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+class DoctrineumFloatExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
     }
 
+    /**
+     * @return string
+     */
     protected function getExternalRootNamespaces()
     {
         $externalRootReflection = new \ReflectionClass(ScalarEnum::class);
